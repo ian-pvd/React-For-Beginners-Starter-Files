9ac072bfcd9a1e93aab67e85b8251d1bbce8e87b
Ian Del Giudice <ian.pvd@gmail.com>
2018-10-15 18:43:03 -0400

    Lesson 14 - Add sample fishes

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -6,14 +6,19 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
+import sampleFishes from '../sample-fishes';
 
 class App extends Component {
   state = {};
 
   addFish = (fish) => {
-    const fishes = {...this.state.fishes};
+    const fishes = { ...this.state.fishes };
     fishes[`fish${Date.now()}`] = fish;
     this.setState({ fishes });
+  }
+
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
   }
 
   render() {
@@ -23,7 +28,7 @@ class App extends Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     );
   }
