b02497475d6b5cd36613b3632b1210f5c77a7c05
Ian Del Giudice <ian.pvd@gmail.com>
2018-11-28 18:04:40 -0500

    16. update order state

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -6,8 +6,13 @@ import React, { Component } from 'react';
 import { formatPrice } from '../helpers';
 
 class Fish extends Component {
+  handleClick = () => {
+    this.props.addToOrder(this.props.index);
+  }
+
   render() {
     const { details } = this.props;
+    const isAvailable = details.status === 'available';
 
     return (
       <li className="menu-fish">
@@ -19,7 +24,13 @@ class Fish extends Component {
         <p>
           {details.desc}
         </p>
-        <button type="submit">Add To Cart</button>
+        <button
+          disabled={!isAvailable}
+          type="submit"
+          onClick={this.handleClick}
+        >
+          {isAvailable ? 'Add To Cart' : 'Sold Out!'}
+        </button>
       </li>
     );
   }
